8cc4e7f299c421069cfb77502ef15f16a6ecd516
GABETROLL <gabriel7icplhappy@gmail.com>
2024-04-25 19:53:36 -0400

    Add Cloud Firestore to the app, and make the models have fromJson/fromSnapshot

--- hoothub/lib/firebase/models/question.dart
+++ hoothub/lib/firebase/models/question.dart
@@ -1,5 +1,5 @@
 class Question {
-  Question({required this.id, required this.question, required this.answers, required this.correctAnswer}) {
+  Question({required this.question, required this.answers, required this.correctAnswer}) {
     if (answers.isEmpty) {
       throw "'answers' argument of 'Question' model can't be empty.";
     }
@@ -8,13 +8,19 @@ class Question {
     }
   }
 
-  final String id;
   final String question;
   final List<String> answers;
   final int correctAnswer;
 
+  static Question fromJson(Map<String, dynamic> data) {
+    return Question(
+      question: data['question'],
+      answers: data['answers'],
+      correctAnswer: data['correctAnswer'],
+    );
+  }
+
   Map<String, dynamic> toJson() => {
-    'id': id,
     'question': question,
     'answers': answers,
     'correctAnswer': correctAnswer,
--- hoothub/lib/firebase/models/test.dart
+++ hoothub/lib/firebase/models/test.dart
@@ -1,11 +1,48 @@
 import 'package:hoothub/firebase/models/question.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Test {
-  const Test({required this.id, required this.name, required this.questions});
+  Test({required this.id, required this.name, required this.questions}) {
+    if (id == '') {
+      throw "`id` argument of `Test` contructor is empty!";
+    }
+    if (name == '') {
+      throw "`id` argument of `Test` contructor is empty!";
+    }
+    if (questions.isEmpty) {
+      throw "`questions` argument of `Test` constructor must have at least one `Question`!";
+    }
+  }
 
   final String id;
   final String name;
   final List<Question> questions;
+
+  /// Returns the `Test` representation of `snapshot.data()`.
+  /// If the data is null, this method returns null.
+  /// If any of the fields are wrong, the constructor call inside this method
+  /// takes care of those errors, and throws them up the stack.
+  static Test? fromSnapshot(DocumentSnapshot<Map<String, dynamic>> snapshot) {
+    Map<String, dynamic>? data = snapshot.data();
+
+    if (data == null) return null;
+
+    if (data['questions'] is! List<Map<String, dynamic>>) {
+      throw "`questions` field of snapshot data is not the correct type!";
+    }
+
+    List<Question> questions = List.from(
+      data['questions'].map(
+        (Map<String, dynamic> question) => Question.fromJson(question),
+      ),
+    );
+
+    return Test(
+      id: data['id'],
+      name: data['name'],
+      questions: questions,
+    );
+  }
 
   Map<String, dynamic> toJson() => {
     'id': id,
