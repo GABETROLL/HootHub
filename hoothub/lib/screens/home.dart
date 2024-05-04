407df2a0be96ddb62c76d9b0e982e45a4d916f91
GABETROLL <gabriel7icplhappy@gmail.com>
2024-05-04 17:58:12 -0400

    Test ViewTest with valid test

--- hoothub/lib/screens/home.dart
+++ hoothub/lib/screens/home.dart
@@ -24,7 +24,7 @@ class Home extends StatelessWidget {
                     question: 'Simplify `sqrt(9)`:',
                     answers: ['sqrt(3)', '3', 'sqrt(9)', '6'],
                     correctAnswer: 1,
-                    secondsDuration: 61
+                    secondsDuration: 60
                   ),
                   Question(
                     question: 'Simplify `sqrt(27)`:',
@@ -36,7 +36,7 @@ class Home extends StatelessWidget {
                     question: 'Simplify `sqrt(10)`:',
                     answers: ['sqrt(10)', 'sqrt(2)sqrt(5)', '3', 'cbrt(100)'],
                     correctAnswer: 0,
-                    secondsDuration: -1
+                    secondsDuration: 1,
                   ),
                 ],
               ),
