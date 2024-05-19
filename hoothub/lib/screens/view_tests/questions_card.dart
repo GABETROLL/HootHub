df94d988071f6b56607910ebd2fad0227a97fda4
GABETROLL <gabriel7icplhappy@gmail.com>
2024-05-18 21:25:53 -0400

    Use ExpansionTile instead

--- hoothub/lib/screens/view_tests/questions_card.dart
+++ hoothub/lib/screens/view_tests/questions_card.dart
@@ -4,79 +4,59 @@ import 'package:flutter/material.dart';
 class QuestionCard extends StatefulWidget {
   const QuestionCard({
     super.key,
-    required this.question,
-    required this.answers,
-    required this.correctAnswer,
+    required this.questionModel,
   });
 
-  final String question;
-  final List<String> answers;
-  final int correctAnswer;
+  final Question questionModel;
 
   @override
   State<QuestionCard> createState() => _QuestionCardState();
 }
+
 class _QuestionCardState extends State<QuestionCard> {
-  bool _open = false;
   bool _correctAnswerRevealed = false;
 
   @override
   Widget build(BuildContext context) {
-    final List<Widget> questionChildren = [
-      Row(
-        children: [
-          Text(widget.question),
-          TextButton(
-            onPressed: () => setState(() {
-              _open = !_open;
-              _correctAnswerRevealed = false;
-              // Closing and opening the answers drawer should hide the correct answers again,
-              // to prevent spoilers.
-            }),
-            child: Text(_open ? 'Close' : 'Open'),
-          ),
-        ],
+    List<Widget> questionChildren = [
+      TextButton(
+        onPressed: () => setState(() {
+          _correctAnswerRevealed = !_correctAnswerRevealed;
+        }),
+        child: Text('${_correctAnswerRevealed ? 'Hide' : 'Reveal'} correct answer'),
       ),
     ];
 
-    if (_open) {
-      questionChildren.add(
-        TextButton(
-          onPressed: () => setState(() {
-            _correctAnswerRevealed = !_correctAnswerRevealed;
-          }),
-          child: Text('${_correctAnswerRevealed ? 'Hide' : 'Reveal'} correct answer'),
-        ),
-      );
+    for (final (int index, String answer) in widget.questionModel.answers.indexed) {
+      final List<Widget> answerChildren = [
+        Text(answer),
+      ];
 
-      for (final (int index, String answer) in widget.answers.indexed) {
-        final List<Widget> answerChildren = [
-          Text(answer),
-        ];
+      if (_correctAnswerRevealed) {
+        final bool currentAnswerCorrect = index == widget.questionModel.correctAnswer;
 
-        if (_correctAnswerRevealed) {
-          final bool currentAnswerCorrect = index == widget.correctAnswer;
-
-          answerChildren.insert(
-            0,
-            Icon(
-              currentAnswerCorrect ? Icons.check : Icons.close,
-              color: currentAnswerCorrect ? Colors.green : Colors.red
-            ),
-          );
-        }
-
-        questionChildren.add(
-          Row(children: answerChildren),
+        answerChildren.insert(
+          0,
+          Icon(
+            currentAnswerCorrect ? Icons.check : Icons.close,
+            color: currentAnswerCorrect ? Colors.green : Colors.red
+          ),
         );
       }
+
+      questionChildren.add(
+        Row(children: answerChildren),
+      );
     }
 
-    return Column(children: questionChildren);
+    return ExpansionTile(
+      title: Text(widget.questionModel.question),
+      children: questionChildren,
+    );
   }
 }
 
-class QuestionsCard extends StatefulWidget {
+class QuestionsCard extends StatelessWidget {
   const QuestionsCard({
     super.key,
     required this.questions,
@@ -85,34 +65,14 @@ class QuestionsCard extends StatefulWidget {
   final List<Question> questions;
 
   @override
-  State<QuestionsCard> createState() => _QuestionsCardState();
-}
-
-class _QuestionsCardState extends State<QuestionsCard> {
-  bool _open = false;
-
-  @override
   Widget build(BuildContext context) {
-    List<Widget> children = [
-      Row(
-        children: [
-          const Text('Questions'),
-          TextButton(
-            onPressed: () => setState(() { _open = !_open; }),
-            child: Text(_open ? 'Close' : 'Open'),
-          ),
-        ],
+    return ExpansionTile(
+      title: const Text('Questions'),
+      children: List<Widget>.from(
+        questions.map<Widget>(
+          (Question question) => QuestionCard(questionModel: question),
+        ),
       ),
-    ];
-
-    if (_open) {
-      for (final Question question in widget.questions) {
-        children.add(
-          QuestionCard(question: question.question, answers: question.answers, correctAnswer: question.correctAnswer),
-        );
-      }
-    }
-
-    return Column(children: children);
+    );
   }
 }
