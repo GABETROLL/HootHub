d085f9aea04f4060ce99b875d360833d33d0ef95
GABETROLL <gabriel7icplhappy@gmail.com>
2024-05-22 15:38:32 -0400

    Remove unused library

--- hoothub/lib/firebase/api/tests.dart
+++ hoothub/lib/firebase/api/tests.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hoothub/firebase/api/auth.dart';
 import 'clients.dart';
 // models
